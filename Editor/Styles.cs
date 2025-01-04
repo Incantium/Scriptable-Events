08ad4a3b70d65eadaf914901c495812747243727
tgerr <vanaest@outlook.com>
2025-01-05 00:56:06 +0100

    Updated tooltip message for the reset button

--- Editor/Styles.cs
+++ Editor/Styles.cs
@@ -3,13 +3,13 @@
 namespace Incantium.Events.Editor
 {
     /// <summary>
-    /// Class representing the different styling used within the scriptable event editor scripts.
+    /// Class representing the different styling used within the event channel editor scripts.
     /// </summary>
     internal static class Styles
     {
         public static readonly GUIContent SEND = new("Send", "Send a message to all event handlers of the event bus.");
         public static readonly GUIContent REQUEST = new("Request", "Request the value from a event handler of the event truck.");
         public static readonly GUIContent REQUEST_ALL = new("Request All", "Request the value from all event handlers of the event truck.");
-        public static readonly GUIContent RESET = new("Reset", "Reset the scriptable event from all event handlers.");
+        public static readonly GUIContent RESET = new("Reset", "Reset the event channel from all event handlers.");
     }
 }
