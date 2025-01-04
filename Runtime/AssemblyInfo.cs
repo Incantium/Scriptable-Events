8f8331cd41155c12cf73ed2a6468e6fbe8cc5285
tgerr <vanaest@outlook.com>
2025-01-05 00:55:21 +0100

    Name change

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -1,1 +1,1 @@
-﻿[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Incantium.ScriptableEvent.Editor")]
+﻿[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Incantium.ScriptableEvents.Editor")]
